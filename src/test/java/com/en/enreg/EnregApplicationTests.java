680187cbc7f5f48e11486f91dd45d34b49d5df71
tarik-guit <tarikkh1993@gmail.com>
2021-01-04 13:58:00 +0100

    by tarik

---
+++ src/test/java/com/en/enreg/EnregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.en.enreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnregApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
